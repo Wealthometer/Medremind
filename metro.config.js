7a9a0e4485da01bcdabf3a87e74ebe77e82fa146
Wealthometer <wealthhajoh87@gmail.com>
2025-09-10 13:20:08 +0100

    Update

---
+++ metro.config.js
@@ -0,0 +1,10 @@
+const { getDefaultConfig } = require("expo/metro-config");
+
+const config = getDefaultConfig(__dirname);
+
+config.resolver.alias = {
+  ...config.resolver.alias,
+  "react-native-svg": "react-native-svg-web",
+};
+
+module.exports = config;
